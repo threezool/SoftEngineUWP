6a407a2e215c5767f4fc75a7f3f264416042257f
Kim Böndergaard <threezool@gmail.com>
2017-04-12 23:35:33 +0200

    Add project files.

---
+++ SoftEngineUWP/Camera.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace SoftEngineUWP
+{
+    internal class Camera
+    {
+        public Vector3 Position { get; set; }
+        public Vector3 Target { get; set; }
+
+    }
+}
