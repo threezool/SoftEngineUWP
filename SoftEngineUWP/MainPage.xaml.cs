f4d99d9e7e59dc773a1ee38e62cac96fc8b1bdd0
Kim Böndergaard <threezool@gmail.com>
2017-04-13 00:31:58 +0200

    Added code for part 3

--- SoftEngineUWP/MainPage.xaml.cs
+++ SoftEngineUWP/MainPage.xaml.cs
@@ -26,7 +26,7 @@ namespace SoftEngineUWP
     public sealed partial class MainPage : Page
     {
         private Device device;
-        Mesh mesh = new Mesh("Cube", 8, 12);
+        Mesh[] meshes;
         Camera mera = new Camera();
 
         public MainPage()
@@ -34,39 +34,16 @@ namespace SoftEngineUWP
             this.InitializeComponent();
         }
 
-        private void Page_Loaded(object sender, RoutedEventArgs e)
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
             // Choose the back buffer resolution here
             WriteableBitmap bmp = new WriteableBitmap(640, 480);
 
-            device = new Device(bmp);
-
             // Our Image XAML control
             frontBuffer.Source = bmp;
 
-            mesh.Vertices[0] = new Vector3(-1, 1, 1);
-            mesh.Vertices[1] = new Vector3(1, 1, 1);
-            mesh.Vertices[2] = new Vector3(-1, -1, 1);
-            mesh.Vertices[3] = new Vector3(1, -1, 1);
-            mesh.Vertices[4] = new Vector3(-1, 1, -1);
-            mesh.Vertices[5] = new Vector3(1, 1, -1);
-            mesh.Vertices[6] = new Vector3(1, -1, -1);
-            mesh.Vertices[7] = new Vector3(-1, -1, -1);
-
-            mesh.Faces[0] = new Face { A = 0, B = 1, C = 2 };
-            mesh.Faces[1] = new Face { A = 1, B = 2, C = 3 };
-            mesh.Faces[2] = new Face { A = 1, B = 3, C = 6 };
-            mesh.Faces[3] = new Face { A = 1, B = 5, C = 6 };
-            mesh.Faces[4] = new Face { A = 0, B = 1, C = 4 };
-            mesh.Faces[5] = new Face { A = 1, B = 4, C = 5 };
-
-            mesh.Faces[6] = new Face { A = 2, B = 3, C = 7 };
-            mesh.Faces[7] = new Face { A = 3, B = 6, C = 7 };
-            mesh.Faces[8] = new Face { A = 0, B = 2, C = 7 };
-            mesh.Faces[9] = new Face { A = 0, B = 4, C = 7 };
-            mesh.Faces[10] = new Face { A = 4, B = 5, C = 6 };
-            mesh.Faces[11] = new Face { A = 4, B = 6, C = 7 };
-
+            device = new Device(bmp);
+            meshes = await device.LoadJSONFileAsync("ms-appx:///Assets/monkey.babylon");
             mera.Position = new Vector3(0, 0, 10.0f);
             mera.Target = Vector3.Zero;
 
@@ -78,11 +55,14 @@ namespace SoftEngineUWP
         {
             device.Clear(0, 0, 0, 255);
 
-            // rotating slightly the cube during each frame rendered
-            mesh.Rotation = new Vector3(mesh.Rotation.X + 0.01f, mesh.Rotation.Y + 0.01f, mesh.Rotation.Z);
+            foreach (var mesh in meshes)
+            {
+                // rotating slightly the meshes during each frame rendered
+                mesh.Rotation = new Vector3(mesh.Rotation.X + 0.01f, mesh.Rotation.Y + 0.01f, mesh.Rotation.Z);
+            }
 
             // Doing the various matrix operations
-            device.Render(mera, mesh);
+            device.Render(mera, meshes);
             // Flushing the back buffer into the front buffer
             device.Present();
         }
